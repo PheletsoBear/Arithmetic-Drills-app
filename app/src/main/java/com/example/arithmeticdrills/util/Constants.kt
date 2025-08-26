f10968bb5f309960a4765ceac3337a9b9e9ce5dc
Pheletso Mashalane <mashalaneevanspheletso@gmail.com>
2025-08-27 01:07:56 +0200

    amended constants

--- app/src/main/java/com/example/arithmeticdrills/util/Constants.kt
+++ app/src/main/java/com/example/arithmeticdrills/util/Constants.kt
@@ -1,10 +1,19 @@
 package com.loc.newsapp.util
 
+import com.loc.newsapp.presentation.navigation.Route
+
 object Constants {
 
+    //SharedPreferences
     const val USER_SETTINGS = "userPreference"
     const val APP_ENTRY = "appEntry"
 
+    //Screens route
+    const val OnBoardingScreenRoute = "OnBoardingScreen"
+    const val HomeScreenRoute = "HomeScreen"
+    const val SessionScreenRoute = "SessionScreen"
+
+    //OnBoarding Constants
     const val OnboardingTitle1 = "Ready to drill?"
     const val OnboardingTitle2 = "Track Progress"
     const val OnboardingTitle3 = "Multiplayer mode"
@@ -14,5 +23,12 @@ object Constants {
     const val OnboardingDesc2 = "View your results after each session and see how you improve"
     const val OnboardingDesc3 = "Share your scores with friends and compete in multiplayer mode"
     const val OnboardingDesc4 = "Toggle and change languages that suit your lingual structure"
+
+    //languages
+    const val SELECT = "Select Language"
+    const val ENGLISH = "English"
+    const val SEPEDI = "Sepedi"
+    const val ZULU = "isiZulu"
+    const val AFRIKAANS = "Afrikaans"
 
 }
