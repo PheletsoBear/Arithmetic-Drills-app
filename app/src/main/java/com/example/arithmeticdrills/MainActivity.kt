052855576a5ca99732d04a35a7af65684cef4c87
Pheletso Mashalane <mashalaneevanspheletso@gmail.com>
2025-08-26 15:04:53 +0200

    amended navGraph

--- app/src/main/java/com/example/arithmeticdrills/MainActivity.kt
+++ app/src/main/java/com/example/arithmeticdrills/MainActivity.kt
@@ -6,27 +6,18 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.arithmeticdrills.ui.theme.ArithmeticDrillsTheme
-import com.loc.newsapp.data.manager.LocalUserManager
 import com.loc.newsapp.presentation.navigation.NavGraph
 import com.loc.newsapp.presentation.navigation.Route
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-    @Inject
-    lateinit var localUserManager: LocalUserManager
 
     val viewModel: MainViewModel by viewModels()
 
@@ -51,8 +42,7 @@ class MainActivity : ComponentActivity() {
                                 Route.HomeScreen.route
                             } else {
                                 Route.OnBoardingScreen.route
-                            },
-                            localUserManager = localUserManager
+                            }
                         )
                     }
                 }
